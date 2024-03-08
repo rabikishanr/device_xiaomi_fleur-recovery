73f8d3890fa33bfcca4b31b604ba23ea3f7ba7d1
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-03-08 16:07:01 +0545

    fleur: add maintainer name

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -43,7 +43,7 @@ if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	        export LC_ALL="C"
 
 		# Version / Maintainer infos
-		export OF_MAINTAINER="Rohit_Tiwari"
+		export OF_MAINTAINER="Rabikishan_Rauniyar"
 		export FOX_VERSION=R12.1
 		export FOX_BUILD_TYPE="Stable"
 
