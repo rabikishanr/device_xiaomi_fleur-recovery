1e656dd3ae640699a6786cbee317fead20a6047a
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-04-02 19:04:41 +0545

    fleur: ADD OMNI support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_fleur.mk
+    $(LOCAL_DIR)/recovery_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_fleur-eng
+    recovery_fleur-eng
