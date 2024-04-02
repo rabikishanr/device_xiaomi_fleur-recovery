a621f8bcd53ac674ad9d33bd6d19e56ad23b88e1
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-04-02 19:03:02 +0545

    fleur: add OMNI support for recovery

--- recovery_fleur.mk
+++ recovery_fleur.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 SebaUbuntu's recovery device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common recovery stuff.
+$(call inherit-product, vendor/recovery/config/common.mk)
 
 # Inherit from fleur device
 $(call inherit-product, device/xiaomi/fleur/device.mk)
 
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := twrp_fleur
+PRODUCT_NAME := recovery_fleur
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi Note 11S/Poco M4 Pro
 PRODUCT_MANUFACTURER := xiaomi
